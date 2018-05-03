a598e416fb8c143337d0696e2dfd39fac802e9a2
DESKTOP-Q0VMKV2\Gagz <singhgagan9999@gmail.com>
2018-05-03 09:04:07 +0530

    commit local storage service changes

--- src/app/webservice/common/localstorage.service.ts
+++ src/app/webservice/common/localstorage.service.ts
@@ -1,4 +1,3 @@
-module StorageHelper {
     export interface IStorageItem {
         key: string;
         value: any;
@@ -23,10 +22,10 @@ module StorageHelper {
         }
 
         // add value to storage
-        add(key: string, item: any) {
+        add(key: string, item: string) {
             if (this.localStorageSupported) {
-                let data = JSON.stringify(item);
-                localStorage.setItem(key, data);
+                //let data = JSON.stringify(item);
+                localStorage.setItem(key, item);
             }
         }
 
@@ -85,83 +84,3 @@ module StorageHelper {
             }
         }
     }
-
-    // custom class for store emails in local storage
-    export class EmailStorage {
-        storageWorker: LocalStorageService;
-
-        // main key that use for store list of emails
-        storageKey: string;
-
-        // list of emails
-        addresses: Array<string>;
-
-        constructor(storageKey: string) {
-            this.storageWorker = new LocalStorageService();
-
-            this.storageKey = storageKey;
-
-            this.addresses = new Array<string>();
-
-            this.activate();
-        }
-
-        // activate custom storage for emails
-        activate() {
-            //this.clear();
-            this.loadAll();
-        }
-
-        // load all emails from storage to list for working with it
-        loadAll() {
-            var storageData = this.storageWorker.get(this.storageKey);
-
-            if (storageData != null && storageData.length > 0) {
-                var emails = JSON.parse(storageData);
-                console.log(emails);
-                if (emails != null) {
-                    this.addresses = emails;
-                }
-                console.log(this.addresses);
-            }
-        }
-
-        // add new email (without duplicate)
-        addEmail(email: string) {
-            if (email.length > 0) {
-
-                // 1. Split email addresses if needed (if we get list of emails)
-                var mas = email.split(/,|;/g);
-                //console.log(mas);
-                // 2. Add each email in the splited list
-                for (var i = 0; i < mas.length; i++) {
-                    // check if not exist and not add new (duplicate)
-                    var index = this.addresses.indexOf(mas[i].trim());
-                    if (index < 0) {
-                        this.addresses.push(mas[i].trim());
-                    }
-                }
-
-                console.log(this.addresses);
-
-                // 3. save to storage
-                this.save();
-            }
-        }
-
-        // clear all data about emails
-        clear() {
-            // remove data by key from storage
-            this.storageWorker.add(this.storageKey, "");
-
-            // or remove with key
-            //this.storageWorker.remove(this.storageKey);
-        }
-
-        // save to storage (save as JSON string)
-        save() {
-            var jsonEmails = JSON.stringify(this.addresses);
-            this.storageWorker.add(this.storageKey, jsonEmails);
-        }
-    }
-} 
